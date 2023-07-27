c4d91f138d68a3bd907520e39a833b91d1874d65
Pangpyo <kkp0639@gmail.com>
2023-07-27 09:30:36 +0900

    🎨 style: userBook entity 코드 정리

--- intobook/src/main/java/com/reboot/intobook/userbook/entity/UserBook.java
+++ intobook/src/main/java/com/reboot/intobook/userbook/entity/UserBook.java
@@ -20,37 +20,30 @@ import java.util.Date;
 @Table( name = "user_book" )
 public class UserBook {
 
-    @Id @Column(name = "user_book_pk") @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userBookPk;
 
-    @Column( name = "user_pk")
     @NotNull
     private Long userPk;
 
-    @Column( name = "isbn")
     @NotNull
     private String isbn;
 
-    @Column(name = "now_page")
     @ColumnDefault("0")
     private int nowPage;
 
-    @Column(name = "created_at", columnDefinition = "default(now())")
+    @Column(columnDefinition = "default(now())")
     @CreationTimestamp
     private Date createdAt;
 
-    @Column(name = "started_at")
     private Date startedAt;
 
-    @Column(name = "completed_at")
     private Date completedAt;
 
-    @Column(name = "is_deleted")
     @ColumnDefault("0")
     @Type(type = "org.hibernate.type.NumericBooleanType")
     private boolean isDeleted;
 
-    @Column(name = "status")
     @Enumerated(EnumType.STRING)
     @NotNull
     private UserBookStatus status;
