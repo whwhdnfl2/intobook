99a63d390b8f22e95f7268d9029d02c4d1464ac6
hyemco <hyemco@gmail.com>
2023-07-30 03:11:08 +0900

    :sparkles: feat: 책 검색 토글 전체 UI
    책 검색 토글 UI 컴포넌트 구현

---
+++ client/intobook/src/assets/img/search/searchToggle.js
@@ -0,0 +1,3 @@
+export const searchIcon = require('./search-icon.png');
+export const barcodehIcon = require('./barcode-icon.png');
+export const bookshIcon = require('./books-icon.png');
--- client/intobook/src/components/bookSearch/BookSearchToggle.js
+++ client/intobook/src/components/bookSearch/BookSearchToggle.js
@@ -1,63 +1,27 @@
 import React from 'react';
-import styled from "styled-components";
+import SearchGroups from './SearchGroups';
+import { searchIcon, barcodehIcon, bookshIcon } from './../../assets/img/search/searchToggle';
+import { Booksearch, BookSearchToggleDiv, Title, TopLine, Line } from '../../styles/BookSearchStyle';
 
-const BookSearchToggle = (props) => {
-  const {
-    spanText1,
-    line6,
-  } = props;
+const BookSearchToggle = () => {
 
 
   return (
-    <div>
-      <Booksearch1>
-        <OverlapGroup>
-        <Text1>
-          <span className="inter-normal-black-20px">{spanText1}</span>
-        </Text1>
-        <Line6 src={line6} alt="Line 6" />
-        </OverlapGroup>
-      </Booksearch1>
-    </div>
+    <Booksearch>
+      <BookSearchToggleDiv>
+      <Title>
+        <span>읽을 책 등록하기</span>
+      </Title>
+      <TopLine />
+      <SearchGroups iconSrc={searchIcon} methodText={'검색하여 등록하기'}></SearchGroups>
+      <Line />
+      <SearchGroups iconSrc={barcodehIcon} methodText={'바코드로 등록하기'}></SearchGroups>
+      <Line />
+      <SearchGroups iconSrc={bookshIcon} methodText={'내 책장에서 등록하기'}></SearchGroups>
+      <Line />
+      </BookSearchToggleDiv>
+    </Booksearch>
   );
 };
-
-const Booksearch1 = styled.div`
-  height: 244px;
-  display: flex;
-  align-items: flex-start;
-  min-width: 364px;
-`;
-
-const OverlapGroup = styled.div`
-  width: 362px;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  padding: 13px 0;
-  align-items: center;
-  min-height: 244px;
-  background-color: var(--white);
-  border-radius: 20px;
-`;
-
-const Text1 = styled.div`
-  font-family: var(--content-font);
-  font-weight: 400;
-  font-size: 20ox;
-  width: 362px;
-  min-height: 24px;
-  margin-right: 0.01px;
-  text-align: center;
-  letter-spacing: 0;
-  line-height: normal;
-`;
-
-const Line6 = styled.img`
-  width: 362px;
-  height: 1px;
-  margin-top: 20px;
-  margin-left: 0;
-`;
 
 export default BookSearchToggle;
