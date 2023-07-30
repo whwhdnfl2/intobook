d1d00181d1c4c2cdf34f7cd7b5768f21664bc4ac
I-HYEON <tjwn1408@naver.com>
2023-07-30 21:12:50 +0900

    ✨ feat: 상단 네비게이션바 아이콘 추가

--- client/intobook/src/components/common/UpperNavbar.js
+++ client/intobook/src/components/common/UpperNavbar.js
@@ -1,10 +1,37 @@
 import react from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
+
+const StyledUpperNavbar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
 
 const UpperNavbar = () => {
     return ( 
-        <div className='UpperNavbar' style={{width:'100%',border:'1px solid black'}}>
-            <h1>UpperNavbar</h1>
-        </div>
+
+        <StyledUpperNavbar>
+            <div className='empty-space'>
+            </div>
+
+            <div className='logo'>
+                BOOK!
+            </div>
+
+            <div className='menu-icons'>
+              <Link to="/tutorials">
+                <FontAwesomeIcon icon={faCircleQuestion} />
+              </Link>
+              
+              <FontAwesomeIcon icon={faBell}/>
+              <FontAwesomeIcon icon={faBars} />
+            </div>
+        </StyledUpperNavbar>
      );
 }
  
