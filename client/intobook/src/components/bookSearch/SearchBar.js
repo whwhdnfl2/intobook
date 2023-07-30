4ae32c8dceffe88bbbe47e0f5413cbde9a49e2b2
hyemco <hyemco@gmail.com>
2023-07-30 22:24:10 +0900

    :sparkles: feat: 책 검색 기능
    책 검색을 위한 /books/list API 통신 확인

--- client/intobook/src/components/bookSearch/SearchBar.js
+++ client/intobook/src/components/bookSearch/SearchBar.js
@@ -1,14 +1,29 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { barcodehIcon, searchIcon } from '../../assets/img/search/searchBottomSheetImg';
 import { Title, SerchBarDiv, BarcordeIcon, Line, SearchBarInput, SearchIcon } from './../../styles/bookSearch/SearchBarStyle';
+import { searchBooks } from './../../api/searchApi';
 
 
 const SearchBar = ({title}) => {
-  const [searchValue, setSearchValue] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState('');
   const searchBarInputRef = useRef(null);
 
   const inputChangeHandler = (event) => {
-    setSearchValue(event.target.value);
+    setSearchKeyword(event.target.value);
+  };
+
+  const searchHandler = async () => {
+    if (searchKeyword.trim() !== '') {
+      // 검색 api 호출
+      const searchValues = await searchBooks(searchKeyword, 0);
+      console.log('결과결과', searchValues)
+    }
+  };
+
+  const enterKeyPressHandler = (event) => { // Enter키로도 검색 가능
+    if (event.key === 'Enter') {
+      searchHandler();
+    }
   };
 
   useEffect(() => { // 검색 페이지로 이동시 input창에 자동 포커스
@@ -23,11 +38,18 @@ const SearchBar = ({title}) => {
         <Line />
         <SearchBarInput 
           placeholder='책 제목, 저자명, 키워드 등을 입력하세요.'
-          value={searchValue}
+          value={searchKeyword}
           onChange={inputChangeHandler}
           ref={searchBarInputRef}
+          onKeyDown={enterKeyPressHandler}
         />
-        {searchValue && <SearchIcon src={searchIcon} alt="search-icon" />}
+        {searchKeyword && (
+          <SearchIcon 
+            onClick={searchHandler}
+            src={searchIcon} 
+            alt="search-icon" 
+          />
+        )}
       </SerchBarDiv>
     </div>
   );
