dc947352a47620ac3f64ac73812a0a60ed74e53d
hyemco <hyemco@gmail.com>
2023-08-01 07:10:12 +0900

    :sparkles: feat: 책 정보 페이지로 라우팅
    ISBN useParams로 받아옴

--- client/intobook/src/pages/BookInfoPage.js
+++ client/intobook/src/pages/BookInfoPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
 
 const BookInfoPage = () => {
+  const { bookId } = useParams();
+
   return (
     <div>
-      책 정보 페이지입니당-!
+      책 정보 페이지입니다-!
+      <p>ISBN: {bookId}</p>
+      {/* 추후 `/books/${bookId}` 으로 API 통신 요청 후 데이터 받아오기*/}
     </div>
   );
 };
