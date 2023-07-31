2628b2cb2973e5c7ffa3498cf89cd32d922d8cc9
hyemco <hyemco@gmail.com>
2023-07-31 15:45:59 +0900

    :sprkles: feat: corsConfig 설정 추가

---
+++ intobook/src/main/java/com/reboot/intobook/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.reboot.intobook.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*");
+    }
+}
