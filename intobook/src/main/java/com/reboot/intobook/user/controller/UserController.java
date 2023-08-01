7e78affc2feef548093a7558ae4869d349d9db36
Minjun Cha <whwhdnfl2@naver.com>
2023-08-01 12:42:25 +0900

    🐛 fix: login 수정 및 명세서에 나와있는데로 url 변경 완료

--- intobook/src/main/java/com/reboot/intobook/user/controller/UserController.java
+++ intobook/src/main/java/com/reboot/intobook/user/controller/UserController.java
@@ -5,7 +5,6 @@ import com.reboot.intobook.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
