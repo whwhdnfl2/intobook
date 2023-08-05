21803f370d007c200350dc25d6410568fb5d125d
I-HYEON <tjwn1408@naver.com>
2023-08-05 19:23:18 +0900

    :sparkles: feat: fcm 라이브러리 설치 및 초기설정

---
+++ client/intobook/src/firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+
+import firebase from 'firebase/app';
+import 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const messaging = firebase.messaging();
