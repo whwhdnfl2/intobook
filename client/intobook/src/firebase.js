01c21fc33f297f967158714b4508b6ac13e1567a
I-HYEON <tjwn1408@naver.com>
2023-08-06 03:06:42 +0900

    :sparkles: feat: 블루투스 fcm 토큰 받기

--- client/intobook/src/firebase.js
+++ client/intobook/src/firebase.js
@@ -1,17 +1,18 @@
-// firebase.js
+// firebase를 초기화하고 firebase 앱 객체를 만듦
+import {initializeApp} from 'firebase/app';
+import {getMessaging} from 'firebase/messaging';
 
-import firebase from 'firebase/app';
-import 'firebase/messaging';
-
+//firebase 구성객체
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAq0vKv3Vxk5akJ04zO5S63DFoYl-qWvaI",
+  authDomain: "intobook-706bb.firebaseapp.com",
+  projectId: "intobook-706bb",
+  storageBucket: "intobook-706bb.appspot.com",
+  messagingSenderId: "666090916041",
+  appId: "1:666090916041:web:f3d1ae6e27f6fc08e3a518"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+export const messaging = getMessaging(app);
 
-export const messaging = firebase.messaging();
--- client/intobook/src/pages/HomePage.js
+++ client/intobook/src/pages/HomePage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Bookmark, Bluetooth, ReadingBook, CheckButton, Timer } from '../components/home';
 import { Layout } from './../styles/CommonStyle';
 import { styled } from 'styled-components';
+import { Button } from '@mui/material';
+import { getToken } from 'firebase/messaging';
+import { messaging } from '../firebase';
 
 const HomePage = () => {
   // HomePage 마운트(?)될 때 
@@ -11,8 +14,27 @@ const HomePage = () => {
   // reocil 전역 상태에서 값 받아오기
   const nickname = '북빠';
 
+  // 권한 허용 시, token 받아오는 함수
+  const requestPermission = () => {
+    console.log('Requesting permission...');
+    //권한을 허용할 것인지 묻는 알림을 띄움. 사용자가 허용시 토큰 받는 로직
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        console.log('Notification permission granted.')
+        getToken(messaging).then((currentToken)=>{
+          if (currentToken){
+            console.log('FCM Token : ',currentToken)
+          } else {
+            console.log('FCM Token Unavailable')
+          }
+        }).catch((err)=>{
+          console.log('error',err);
+        })
+      }})};
+
   return (
     <Layout>
+      <Button onClick={requestPermission}>bluetooth</Button>
       <WelcomeText>Hello, {nickname}님-!</WelcomeText>
       <CheckButton>확인</CheckButton>
       <Bluetooth />
