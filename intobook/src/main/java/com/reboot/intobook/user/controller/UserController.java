b236875e7b97d61e6583f4bd9e8a4f6d3b6ce666
Minjun Cha <whwhdnfl2@naver.com>
2023-08-05 19:14:47 +0900

    ✨ feat: @ApiOperation()을 통해서 api 설명 완료, JwtUtil에서 userPk나 mail을 더 쉽게 추출하게 많듧, 오타수정

--- intobook/src/main/java/com/reboot/intobook/user/controller/UserController.java
+++ intobook/src/main/java/com/reboot/intobook/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.reboot.intobook.fcm.FCMService;
 import com.reboot.intobook.user.service.UserService;
 import com.reboot.intobook.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +27,9 @@ public class UserController {
 
     //닉네임 업데이트
     @PatchMapping("/updateNickname")
+    @ApiOperation(value="닉네임 바꾼다")
     public ResponseEntity<?> updateNickname(@RequestHeader("Authorization")String accessToken, @RequestParam String nickname){
-        //claim을 jwtUtil로 추출한다. 그리고 해당 claim에서 userPk를 추출한다.
-        Claims claims = jwtUtil.extractClaims(accessToken);
-        Long userPk = claims.get("userPk", Long.class);
+        Long userPk = jwtUtil.getUserPkFromAccessToken(accessToken);
         try{
             userService.updateNickname(userPk, nickname);
             return new ResponseEntity<>(HttpStatus.OK);
@@ -39,10 +39,9 @@ public class UserController {
     }
     //로그아웃. 해당 유저의 fcmToken을 제거한다.
     @GetMapping("/logout")
+    @ApiOperation(value="로그아웃, fcm토큰 제거")
     public ResponseEntity<?> logout(@RequestHeader("Authorization")String accessToken){
-        //claim을 jwtUtil로 추출한다. 그리고 해당 claim에서 userPk를 추출한다.
-        Claims claims = jwtUtil.extractClaims(accessToken);
-        Long userPk = claims.get("userPk", Long.class);
+        Long userPk = jwtUtil.getUserPkFromAccessToken(accessToken);
         try{
             userService.deleteFcmToken(userPk);
             return new ResponseEntity<>(HttpStatus.OK);
@@ -53,7 +52,8 @@ public class UserController {
 
     //fcmToken으로 알람 보내는 테스트
     @GetMapping("/fcmTest")
-    public ResponseEntity<?> fcmTest(@RequestHeader("Authorization")String accessToken){
+    @ApiOperation(value="fcm알림 테스트")
+            public ResponseEntity<?> fcmTest(@RequestHeader("Authorization")String accessToken){
         try{
             fcmService.test(accessToken);
             return  new ResponseEntity<>(HttpStatus.OK);
@@ -64,11 +64,10 @@ public class UserController {
     }
 
     //사용자가 알림을 허용하면 fcmToken을 받아서 저장한다.
-    @PostMapping("/tramsmitFcmToken")
-    public ResponseEntity<?> tramsmitFcmToken(@RequestHeader("Authorization")String accessToken, @RequestBody String fcmToken){
-        //claim을 jwtUtil로 추출한다. 그리고 해당 claim에서 userPk를 추출한다.
-        Claims claims = jwtUtil.extractClaims(accessToken);
-        Long userPk = claims.get("userPk", Long.class);
+    @PostMapping("/transmitFcmToken")
+    @ApiOperation(value="transmitFcmToken을 수신한다")
+    public ResponseEntity<?> transmitFcmToken(@RequestHeader("Authorization")String accessToken, @RequestBody String fcmToken){
+        Long userPk = jwtUtil.getUserPkFromAccessToken(accessToken);
         try{
             userService.insertFcmToken(userPk, fcmToken);
             return new ResponseEntity<>(HttpStatus.OK);
--- intobook/src/main/java/com/reboot/intobook/utils/JwtUtil.java
+++ intobook/src/main/java/com/reboot/intobook/utils/JwtUtil.java
@@ -7,6 +7,18 @@ public class JwtUtil {
 
     private final String secretKey = "sc136as1c5qw4c13ax1cz3x5c4as3c1a35scczx3casc54";
 
+    public Long getUserPkFromAccessToken(String token){
+        Claims claims = extractClaims(token);
+        Long userPk = claims.get("userPk", Long.class);
+        return userPk;
+    }
+
+    public String getEmailFromAccessToken(String token){
+        Claims claims = extractClaims(token);
+        String email = claims.get("email", String.class);
+        return email;
+    }
+
     // JWT 토큰에서 클레임 추출
     public Claims extractClaims(String token) {
         token = token.substring(7);
