37fa1ce90765b65ebca6fe79b336dfe8b8afcc65
SEOIHYEON <tjwn1408@naver.com>
2023-08-07 00:24:06 +0900

    :sparkles: feat: 책장 기본 기능 구현

--- client/intobook/src/components/bookShelves/BookshelvesContent.js
+++ client/intobook/src/components/bookShelves/BookshelvesContent.js
@@ -1,21 +1,19 @@
 import React, {useEffect, useState} from 'react';
-import { Button, FormControl, MenuItem, Select } from '@mui/material';
+import { FormControl, MenuItem, Select } from '@mui/material';
 import { userbooks } from '../../api/userbookApi';
+import { useRecoilState } from 'recoil';
+import { UserBooksAtom } from '../../recoil/user/UserAtom';
+import BookCover from './../common/bookCover';
 
 const BookshelvesContent = ({ selectedTab }) => {
   const [filter, setFilter] = useState('startedAt'); // 기본 정렬 기준은 등록순
-  //필터가 바뀔 때마다 실행되는 함수
-  const handleFilterChange = (event) => {
-    // setState로 filter 값을 바꿈. 비동기적으로 작동하므로, useEffect에서 api 호출
-    setFilter((prev) => event.target.value);
-    // console.log('이 때 출력해보면, 아직 filter값이 바뀌어있지 않음',filter)
-  };
+  const [userBooks, setUserBooks] = useRecoilState(UserBooksAtom)
 
   const fetchBooks = async () => {
     // api 호출
     try {
-      const response = await userbooks(filter,0)
-      console.log(response)
+      const response = await userbooks(filter,0,selectedTab);
+      setUserBooks((prev) => response);
     } catch (err) {
       console.log(err);
     }
@@ -23,12 +21,19 @@ const BookshelvesContent = ({ selectedTab }) => {
 
   useEffect(() => {
     fetchBooks();
-    console.log('여기서 출력해보면 filter 값이 잘 바뀌어있음',filter)
+    // console.log('여기서 출력해보면 filter 값이 잘 바뀌어있음',filter)
+    console.log('값확인',selectedTab)
+    console.log('확인!',userBooks)
   }, [filter, selectedTab]);
+
+  //필터가 바뀔 때마다 실행되는 함수
+  const handleFilterChange = (event) => {
+    // setState로 filter 값을 바꿈. 비동기적으로 작동하므로, useEffect에서 api 호출
+    setFilter((prev) => event.target.value);
+  };
 
   return (
     <div>
-      {/* 필터 부분 */}
       <FormControl>
         <Select value={filter} onChange={handleFilterChange}>
           <MenuItem value="startedAt">등록순</MenuItem>
@@ -38,9 +43,14 @@ const BookshelvesContent = ({ selectedTab }) => {
         </Select>
       </FormControl>
       
-      {/* 책들 부분 */}
       <div>
-        {/* 책 목록을 표시하는 로직을 구현하세요 */}
+        {userBooks.length > 0 ? (
+          userBooks.map((book) => (
+        <BookCover key={book.userBookPk} image={book.coverImage} alt={"책이미지"}/>
+        ))
+      ) : (
+        <p>책이 없습니다.</p>
+      )}
       </div>
     </div>
   );
