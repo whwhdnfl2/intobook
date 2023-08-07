311e22ac90fbd976bff41bbde8ac6bf375e3b5bf
유혜민 <hyemco@gmail.com>
2023-08-07 14:58:18 +0900

    :sparkles: feat: 프로그레스바 컴포넌트 변경
    메인 페이지, 책 정보 페이지 프로그레스바 컴포넌트 사용

--- client/intobook/src/components/common/progressBar.js
+++ client/intobook/src/components/common/progressBar.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import LinearProgress from '@mui/material/LinearProgress';
-import Box from '@mui/material/Box';
+import { styled } from 'styled-components';
 
-function LinearProgressWithLabel(props) {
-  return (
-    <Box sx={{ display: 'flex', alignItems: 'center', margin: '20px'}}>
-      <Box sx={{ width: '100%', mr: 1 }}>
-        <LinearProgress  variant="determinate" value={props.value} />
-      </Box>
-      <Box sx={{ minWidth: 10 }}>
-      </Box>
-    </Box>
-  );
-}
+const ProgressBar = ({ progress, containerWidth }) => {
+  const fillerStyles = {
+    height: '100%',
+    width: `${progress}%`,
+    backgroundColor: '#F5911C',
+    borderRadius: 'inherit',
+    textAlign: 'right',
+  }
 
-export default function ProgressBar({ now_page = 0 }) {
   return (
-    <Box sx={{ width: '100%' }}>
-      <LinearProgressWithLabel value={now_page} />
-    </Box>
+    <div style={{ width: `${containerWidth}px`, margin: '0 auto' }}>
+      <Progress>{`${progress}% 다 왔어요`}</Progress>
+      <ContainerDiv>
+        <div style={fillerStyles}></div>
+      </ContainerDiv>
+    </div>
   );
-}
+};
+
+const Progress = styled.div`
+  color: var(--gray);
+  font-family: var(--main-font);
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-bottom: 3px;
+  text-align: right;
+`;
+
+const ContainerDiv = styled.div`
+  height: 16px;
+  width: 100%;
+  background-color: #D9D9D9;
+  border-radius: 50px;
+`;
+
+export default ProgressBar;
