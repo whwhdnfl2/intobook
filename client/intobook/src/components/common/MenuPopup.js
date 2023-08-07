eae28ad6fda947d361904cf1addf8af78f30662a
hyemco <hyemco@gmail.com>
2023-08-08 06:52:41 +0900

    :sparkles: feat: 히스토리 로그 삭제
    MenuPopup 눌러 삭제하기 누르면 로그 삭제
    삭제 후 HistoryLogsAtom 갱신하여 삭제된 상태 바로 확인 가능하게 함

--- client/intobook/src/components/common/MenuPopup.js
+++ client/intobook/src/components/common/MenuPopup.js
@@ -1,9 +1,26 @@
 import React, { useState } from 'react';
-import { Menu, MenuItem } from '@mui/material';
 import Modal from './Modal';
+import { deleteBookHistory } from '../../api/historyApi';
+import { Menu, MenuItem } from '@mui/material';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { HistoryLogsAtom, LogAtom } from '../../recoil/book/BookAtom';
 
 const MenuPopup = ({ anchorEl, open, onClose }) => {
-  const [openModal, setOpenModal] = useState(false); 
+  const [openModal, setOpenModal] = useState(false);
+  const log = useRecoilValue(LogAtom);
+  const [historyLog, setHistoryLog] = useRecoilState(HistoryLogsAtom);
+
+  const deleteLogHandler = async () => {
+    try {
+      await deleteBookHistory(log.historyPk);
+      const updatedHistoryLog = historyLog.filter(item => item.historyPk !== log.historyPk);
+      setHistoryLog(updatedHistoryLog);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      onClose();
+    }
+  };
 
   return (
     <>
@@ -25,7 +42,8 @@ const MenuPopup = ({ anchorEl, open, onClose }) => {
         }}
       >
         <MenuItem onClick={() => { onClose(); setOpenModal(true); }} sx={{ fontSize: '12px' }} >수정하기</MenuItem>
-        <MenuItem onClick={onClose} sx={{ fontSize: '12px' }}>삭제하기</MenuItem>
+        {/* <MenuItem onClick={onClose} sx={{ fontSize: '12px' }}>삭제하기</MenuItem> */}
+        <MenuItem onClick={deleteLogHandler} sx={{ fontSize: '12px' }}>삭제하기</MenuItem>
       </Menu>
       <Modal openModal={openModal} setOpenModal={setOpenModal} modalType={'LogEdit'} />
     </>
