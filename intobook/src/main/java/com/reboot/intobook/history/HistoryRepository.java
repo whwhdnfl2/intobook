077442033518c9eb27d7763b06a5c6a605c74ffa
정명진 <jmj2588547@naver.com>
2023-08-08 17:43:17 +0900

    :sparkles: feat: 통계 Service 개발 이전에 Repository 수정

--- intobook/src/main/java/com/reboot/intobook/history/HistoryRepository.java
+++ intobook/src/main/java/com/reboot/intobook/history/HistoryRepository.java
@@ -18,5 +18,6 @@ public interface HistoryRepository extends JpaRepository<History, Long> {
 //    h.startTime, h.endTime, h.comment, h.pageAmount
 
     List<GetHistoryResponse> findByUser(User user);
+    List<GetHistoryResponse> findByUserPk(Long userPk);
 
 }
