18ddeabdc27c3343a055a1e0ab6f4ccb58187b66
zzarru <sn20180301@gmail.com>
2023-08-08 10:24:59 +0900

    :memo: docs: 프론트 폴더 정리

--- client/intobook/src/pages/StatisticsPage.js
+++ client/intobook/src/pages/StatisticsPage.js
@@ -2,7 +2,7 @@ import BookCharacter from "../components/userStatistics/BookCharacter";
 
 const StatisticsPage = () => {
     const username = "zzarru"
- 
+
     return ( 
     <div>
         {username} 오늘도 피그마만 했다.
