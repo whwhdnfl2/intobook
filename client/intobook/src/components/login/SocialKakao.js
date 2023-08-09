4a122a411c5d38d1dc98d08c9d4f9ada8a7f8d64
Pangpyo <kkp0639@gmail.com>
2023-08-09 17:18:08 +0900

    👷 chore: 빌드관련 uri 분리

--- client/intobook/src/components/login/SocialKakao.js
+++ client/intobook/src/components/login/SocialKakao.js
@@ -5,7 +5,7 @@ import { IsLoggedIn } from '../../recoil/user/UserAtom';
 
 const SocialKakao = () => {
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(IsLoggedIn);
-  const kakaoURL = `http://localhost:8080` 
+  const kakaoURL = process.env.REACT_APP_API_URL;
 
   const handleLogin = () => {
       window.location.href = kakaoURL;
