a8639b6d67f74c07bca11c1f175b71427a50bd80
Pangpyo <kkp0639@gmail.com>
2023-08-09 16:26:48 +0900

    👷 chore: 환경별 엔드포인트 설정

--- intobook/src/main/java/com/reboot/intobook/config/WebConfig.java
+++ intobook/src/main/java/com/reboot/intobook/config/WebConfig.java
@@ -1,12 +1,23 @@
 package com.reboot.intobook.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${preEndPoint}")
+    public String preEndPoint;
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix(preEndPoint, HandlerTypePredicate.forAnnotation(RestController.class));
+    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
