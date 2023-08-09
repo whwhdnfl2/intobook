64578d7fc8c8c3395b4e3cadda3f251ece01ee3d
Pangpyo <kkp0639@gmail.com>
2023-08-09 14:54:04 +0900

    👷 chore: 빌드 환경변수로 리다이렉트 url설정

--- intobook/src/main/java/com/reboot/intobook/user/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ intobook/src/main/java/com/reboot/intobook/user/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -4,6 +4,7 @@ import com.reboot.intobook.user.jwt.JwtService;
 import com.reboot.intobook.user.oauth2.CustomOAuth2User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -33,12 +34,16 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     }
 
     // TODO : 소셜 로그인 시에도 무조건 토큰 생성하지 말고 JWT 인증 필터처럼 RefreshToken 유/무에 따라 다르게 처리해보기
+
+    @Value("${redirect.url}")
+    private String url;
     private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User, String fcmToken) throws IOException {
         String accessToken = jwtService.createAccessToken(oAuth2User.getEmail(), oAuth2User.getUserPk());
         String refreshToken = jwtService.createRefreshToken();
+        log.info("리다이렉트 " +url);
         response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
         response.addHeader(jwtService.getRefreshHeader(), "Bearer " + refreshToken);
-        response.sendRedirect("http://localhost:3000?accessToken=" + "Bearer " + accessToken + "&refreshToken=" + "Bearer " + refreshToken);
+        response.sendRedirect(url + "?accessToken=" + "Bearer " + accessToken + "&refreshToken=" + "Bearer " + refreshToken);
         jwtService.updateRefreshTokenAndFcmToken(oAuth2User.getEmail(), refreshToken, fcmToken);
     }
 }
