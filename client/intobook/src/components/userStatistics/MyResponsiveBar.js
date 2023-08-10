9d9c0a702176f3e01e0ea9b07295c372d59d5cd9
zzarru <sn20180301@gmail.com>
2023-08-10 17:22:56 +0900

    :sparkles: feat: barchart 구현

--- client/intobook/src/components/userStatistics/MyResponsiveBar.js
+++ client/intobook/src/components/userStatistics/MyResponsiveBar.js
@@ -1,127 +1,119 @@
 import React from 'react';
 import { ResponsiveBar } from '@nivo/bar'
 
-
 const MyResponsiveBar = ({ data }) => (
-    <ResponsiveBar
-        data={data}
-        keys={[
-            'hot dog',
-            'burger',
-            'sandwich',
-            'kebab',
-            'fries',
-            'donut'
-        ]}
-        indexBy="country"
-        margin={{ top: 72, right: 120, bottom: 72, left: 48 }}
-        padding={0.2}
-        groupMode="grouped"
-        valueScale={{ type: 'linear' }}
-        indexScale={{ type: 'band', round: true }}
-        colors={{ scheme: 'pastel1' }}
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: '#38bcb2',
-                size: 4,
-                padding: 1,
-                stagger: true
-            },
-            {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: '#eed312',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
+
+  <ResponsiveBar
+    data={data}
+    keys={[
+      'lastweek',
+      'thisweek'
+    ]}
+    indexBy="day"
+    margin={{ top: 24, right: 0, bottom: 72, left: 60 }}
+    padding={0.2}
+    // width={320}
+    height={300}
+    minValue={0}
+    // maxValue={0}
+    groupMode="grouped"
+    valueScale={{ type: 'linear' }}
+    indexScale={{ type: 'band', round: true }}
+    colors={{ scheme: 'pastel1' }}
+    defs={[
+      {
+        id: 'lines',
+        type: 'lines',
+        background: 'inherit',
+        color: '#eed312',
+        rotation: 0,
+        lineWidth: 6,
+        spacing: 10
+      }
+    ]}
+    fill={[
+      {
+        match: {
+          id: 'lastweek'
+        },
+        id: 'lines'
+      },
+      {
+        match: {
+          id: 'thisweek'
+        },
+        id: 'lines'
+      }
+    ]}
+    borderRadius={5}
+    borderColor={{
+      from: 'color',
+      modifiers: [
+        [
+          'darker',
+          1.6
+        ]
+      ]
+    }}
+    // axisBottom={{
+    //     tickSize: 5,
+    //     tickPadding: 5,
+    //     tickRotation: 0,
+    //     legend: 'country',
+    //     legendPosition: 'middle',
+    //     legendOffset: 32
+    // }}
+    axisLeft={null}
+    axisBottom={null}
+    // axisLeft={{
+    //     tickSize: 5,
+    //     tickPadding: 5,
+    //     tickRotation: 0,
+    //     legend: '읽은 시간',
+    //     legendPosition: 'middle',
+    //     legendOffset: 0
+    // }}
+    enableLabel={false}
+    enableGridY={false}
+    labelSkipWidth={12}
+    labelSkipHeight={12}
+    labelTextColor={{
+      from: 'color',
+      modifiers: [
+        [
+          'darker',
+          1.6
+        ]
+      ]
+    }}
+    legends={[
+      {
+        dataFrom: 'keys',
+        anchor: 'top-right',
+        direction: 'column',
+        justify: false,
+        translateX: 120,
+        translateY: 0,
+        itemsSpacing: 2,
+        itemWidth: 100,
+        itemHeight: 20,
+        itemDirection: 'left-to-right',
+        itemOpacity: 0.85,
+        symbolSize: 20,
+        effects: [
+          {
+            on: 'hover',
+            style: {
+              itemOpacity: 1
             }
-        ]}
-        fill={[
-            {
-                match: {
-                    id: 'fries'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'sandwich'
-                },
-                id: 'lines'
-            }
-        ]}
-        borderRadius={3}
-        borderColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    1.6
-                ]
-            ]
-        }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'country',
-            legendPosition: 'middle',
-            legendOffset: 32
-        }}
-        axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'food',
-            legendPosition: 'middle',
-            legendOffset: -40
-        }}
-        enableLabel={false}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    1.6
-                ]
-            ]
-        }}
-        legends={[
-            {
-                dataFrom: 'keys',
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 120,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 20,
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
-        role="application"
-        ariaLabel="Nivo bar chart demo"
-        barAriaLabel={e=>e.id+": "+e.formattedValue+" in country: "+e.indexValue}
-    />
+          }
+        ]
+      }
+    ]}
+    role="application"
+    ariaLabel="Nivo bar chart demo"
+    barAriaLabel={e => e.id + ": " + e.formattedValue + " in country: " + e.indexValue}
+  />
 )
 
 export default MyResponsiveBar;
--- client/intobook/src/pages/StatisticsPage.js
+++ client/intobook/src/pages/StatisticsPage.js
@@ -1,13 +1,18 @@
 import { AverageStatistics } from "../components/common";
-import {BookCharacter, TotalStatistic, WeeklyStatistic, MyResponsiveBar} from './../components/userStatistics';
+import {BookCharacter, TotalStatistic, WeeklyStatistic} from './../components/userStatistics';
 
-
+// const data = [
+//     { day: '월', lastweek: 800, thisweek: 30 },
+//     { day: '화', lastweek: 520, thisweek: 0 },
+//     { day: '수', lastweek: 33, thisweek: 2 },
+//     { day: '목', lastweek: 520, thisweek: 100 },
+//     { day: '금', lastweek: 4, thisweek: 24 },
+//     { day: '토', lastweek: 10, thisweek: 55 },
+//     { day: '일', lastweek: 520, thisweek: 1 }
+// ];
 
 const StatisticsPage = () => {
     const username = "zaru"
-    const data = [{country: 'USA', 'hot dog': 25, 'burger': 30, 'sandwitch':40, 'kebab':40, 
-    'fries':40, 'donut':40 }];
-    console.log(data)
 
     return (
         <div>
@@ -15,7 +20,7 @@ const StatisticsPage = () => {
             <BookCharacter />
             <TotalStatistic />
             <WeeklyStatistic />
-            <MyResponsiveBar data={data}  />
+            {/* <MyResponsiveBar data={data} /> */}
             <AverageStatistics readingTime={'25'} readSpeed={'31'} />
         </div>
     );
