40a7f8c5de1e5881032e0e034ef7afb5591de841
hyemco <hyemco@gmail.com>
2023-08-11 00:09:38 +0900

    :sparkles: feat: 완독 안내 모달 컴포넌트 구현
    progress 95 이상일 경우 완독 안내 모달 띄우기
    모달 닫았을 경우 localstorage에 값을 저장해 반복해서 띄우지 않도록 함
    96% 이상일 때만 프로그레스바 클릭가능, 클릭하면 완독 안내 모달 띄움

--- client/intobook/src/components/home/ReadingBook.js
+++ client/intobook/src/components/home/ReadingBook.js
@@ -1,45 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ProgressBar, BookCover, Modal } from './../common';
 import SearchBottomSheet from './../bookSearch/SearchBottomSheet';
+import CurrentBookStatus from './CurrentBookStatus';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { BluetoothAtom, BookmarkStatusAtom, ReadingBookAtom } from './../../recoil/bookmark/bookmarkAtom';
+import { getReadingBookInfo } from '../../api/userbookApi';
 import { StyledEngineProvider, Container, Box } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import { useRecoilValue } from 'recoil';
-import { BluetoothAtom, BookmarkStatusAtom, ReadingBookAtom } from './../../recoil/bookmark/bookmarkAtom';
-import CurrentBookStatus from './CurrentBookStatus';
 import { styled } from 'styled-components';
 
 const ReadingBook = () => {
   const [isOpen, setIsOpen] = useState(false);
   const isConnected = useRecoilValue(BluetoothAtom);
   const isBookmarkOut = useRecoilValue(BookmarkStatusAtom);
-  const nowReadingBook = useRecoilValue(ReadingBookAtom);
+  const [openBookInfoModal, setOpenBookInfoModal] = useState(false);
+  const [openCompleteBookModal, setOpenCompleteBookModal] = useState(false);
+  const [nowReadingBook, setNowReadingBook] = useRecoilState(ReadingBookAtom);
 
-  const [openModal, setOpenModal] = useState(false);
+  useEffect(() => {
+    const getReadingBook = async () => {
+      const detailInfo = await getReadingBookInfo();
+      setNowReadingBook(detailInfo);
 
-  const closeModal = () => {
-    setOpenModal(false);
+      // 진행률 95% 이상일 때 모달 띄우기
+      if (detailInfo?.nowPage && detailInfo?.page) {
+        const nowPage = detailInfo.nowPage + 30;
+        const progress = Math.floor((nowPage / detailInfo.page) * 100);
+
+        if (progress >= 95) {
+          // localStorage 값 설정
+          const modalVal = localStorage.getItem('hasCloseCompleteBookModal')
+          if (modalVal === null) {
+            localStorage.setItem('hasCloseCompleteBookModal', 'false');
+          }
+          // localStorage 값이 true가 아닐 때만 모달 띄우기
+          if (modalVal !== 'true') {
+            setOpenCompleteBookModal(true);
+          }
+        }
+      }
+    };
+    getReadingBook();
+
+  }, [setNowReadingBook]);
+
+  const closeBookInfoModal = () => {
+    setOpenBookInfoModal(false);
   };
 
-  const coverImg = nowReadingBook?.coverImage;
-  const userBookId = nowReadingBook?.userBookPk;
-  const nowPage = nowReadingBook?.nowPage + 30;
-  const progress = Math.floor((nowPage / nowReadingBook?.page) * 100);
+  const closeCompleteBookModal = () => {
+    setOpenCompleteBookModal(false);
+  };
 
   const searchHandler = () => {
     if (isConnected && isBookmarkOut) {
       // 안내 모달 띄우기
-      setOpenModal(true);
+      openBookInfoModal(true);
     } else {
       setIsOpen(true);
     }
   };
 
+  const coverImg = nowReadingBook?.coverImage;
+  const userBookId = nowReadingBook?.userBookPk;
+  const nowPage = nowReadingBook?.nowPage + 90;
+  const progress = nowReadingBook?.page ? Math.floor((nowPage / nowReadingBook.page) * 100) : 0;
+
   return (
     <>
       <StyledEngineProvider injectFirst>
         <GridContainer>
-          <CurrentBook sx={{ background: nowReadingBook? '#859FF8' : 'var(--white)' }}>
+          <CurrentBook sx={{ background: nowReadingBook ? '#859FF8' : 'var(--white)' }}>
             <Link to={`/userbook/${userBookId}`} style={{ textDecoration: 'none' }}>
               {nowReadingBook && <BookCover image={coverImg} customStyle={{ border: '2px solid white' }} />}
             </Link>
@@ -52,10 +84,16 @@ const ReadingBook = () => {
           </CurrentBook>
           <CurrentBookStatus />
         </GridContainer>
-        {nowReadingBook && <ProgressBar progress={progress} containerWidth={320} bbg={'#D9D9D9'} />}
+        <div
+          style={{ cursor: progress >= 95 ? 'pointer' : 'default' }}
+          onClick={() => { if (progress >= 95) setOpenCompleteBookModal(true); }}
+        >
+          {nowReadingBook && <ProgressBar progress={progress} containerWidth={320} bbg={'#D9D9D9'} />}
+        </div>
       </StyledEngineProvider>
       <SearchBottomSheet isOpen={isOpen} setIsOpen={setIsOpen} clickHandler={searchHandler} />
-      <Modal openModal={openModal} setOpenModal={setOpenModal} modalType={'bookmarkInfo'} closeModal={closeModal} height={'240px'} />
+      <Modal openModal={openBookInfoModal} setOpenModal={setOpenBookInfoModal} modalType={'bookmarkInfo'} closeModal={closeBookInfoModal} height={'240px'} />
+      <Modal openModal={openCompleteBookModal} setOpenModal={setOpenCompleteBookModal} modalType={'completeBook'} closeModal={closeCompleteBookModal} height={'160px'} />
     </>
   );
 };
