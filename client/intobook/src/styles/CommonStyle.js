a15de8fdd28a78989e8c4c39a3aa27f36bf546a7
유혜민 <hyemco@gmail.com>
2023-08-11 10:28:50 +0900

    :lipstick: design: 기본 폰트 px -> rem으로 변경
    BottomSheet 크기 변경

--- client/intobook/src/styles/CommonStyle.js
+++ client/intobook/src/styles/CommonStyle.js
@@ -4,7 +4,8 @@ export const Layout = styled.div`
   display: flex;
   flex-direction: column;
   height: 77vh;
-  margin-right: 10px;
-  margin-left: 10px;
-  /* border: 1px solid black;*/
+  margin: 0 1.25rem;
+  // margin-right: 10px;
+  // margin-left: 10px;
+  // border: 1px solid black;
 `;
