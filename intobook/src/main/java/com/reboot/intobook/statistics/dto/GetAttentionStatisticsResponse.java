cbdc8f87cd5c3344087d2566adb238492bccff01
김광표 <kkp0639@gmail.com>
2023-08-13 01:46:29 +0900

    ✨ feat: 멀티리드, 버닝 로직 수정

--- intobook/src/main/java/com/reboot/intobook/statistics/dto/GetAttentionStatisticsResponse.java
+++ intobook/src/main/java/com/reboot/intobook/statistics/dto/GetAttentionStatisticsResponse.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Builder
 public class GetAttentionStatisticsResponse {
     private int attention;
-    private double multiRead;
+    private boolean multiRead;
     private boolean isBurning;
     private WeekDay mostActiveWeekDay;
     private ActiveTime mostActiveTime;
--- intobook/src/main/java/com/reboot/intobook/statistics/entity/WeekDay.java
+++ intobook/src/main/java/com/reboot/intobook/statistics/entity/WeekDay.java
@@ -1,5 +1,5 @@
 package com.reboot.intobook.statistics.entity;
 
 public enum WeekDay {
-    월요알, 화요일, 수요일, 목요일, 금요일, 토요일, 일요일
+    월요일, 화요일, 수요일, 목요일, 금요일, 토요일, 일요일
 }
