76313014814b6a30d9cdae1416a96da1c4cd8c72
SEOIHYEON <tjwn1408@naver.com>
2023-08-14 17:07:19 +0900

    :sparkles: feat : 알림 서비스 설정 구현

--- client/intobook/config-overrides.js
+++ client/intobook/config-overrides.js
@@ -1,20 +1,14 @@
 // config-overrides.js
 module.exports = function override(config, env) {
     // 여기에 원하는 웹팩 설정을 추가하거나 수정합니다.
-    if (env === 'development') {
-        config.devServer = {
-            ...config.devServer,
-            port: 3000,
-            liveReload: true,
-            host: "localhost",
-            allowedHosts: "all",
-            open: true,
-            client: {
-              overlay: true,
-              webSocketURL: { hostname: "localhost", port: 3000 },
-            },
-            compress: true,
-          };
-      }
+    if (env === 'production') { // 배포 환경
+      config.devServer = undefined; // 배포 환경에서는 개발 서버 설정 필요 없음
+      config.output.publicPath = "/"; // 배포된 파일의 경로를 설정
+  
+      config.optimization = {
+        ...config.optimization,
+        minimize: true, // 빌드 결과물을 압축
+      };
+    }
     return config;
   };  
