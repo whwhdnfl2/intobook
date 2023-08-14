f7b8a81c7768522180e57ec01b0305de5e3ba3c9
zzarru <sn20180301@gmail.com>
2023-08-14 12:46:13 +0900

    :sparkles: feat: 닉네임 수정 빈값 prevent 설정 및 enter 입력 활성화

--- client/intobook/src/pages/BookshelvesPage.js
+++ client/intobook/src/pages/BookshelvesPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Typography} from '@mui/material';
+import { Typography } from '@mui/material';
 import BookshelvesMain from '../components/bookShelves/BookshelvesMain';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
@@ -10,12 +10,12 @@ import { UserNameAtom } from '../recoil/user/UserAtom';
 
 const BookshelvesPage = () => {
   const username = useRecoilValue(UserNameAtom);
-  
+
   return (
     <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
       <Layout>
         <StyleSecondContainer>
