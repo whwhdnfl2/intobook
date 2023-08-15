3e85a1f5d3a6ce761e2677d6e30d026728b01060
SEOIHYEON <tjwn1408@naver.com>
2023-08-16 02:59:58 +0900

    :sparkles: feat: 웹팩 설정 변경

--- client/intobook/config-overrides.js
+++ client/intobook/config-overrides.js
@@ -1,6 +1,4 @@
 module.exports = function override(config, env) {
-  //이전의 설정을 그대로 가져오기...
-  const originalConfig = require(paths.scriptVersion + '/config/webpack.config.' + env);
 
   // 기존 설정을 수정하거나 추가
   config.devServer = {
@@ -19,5 +17,26 @@ module.exports = function override(config, env) {
     compress: true,
   };
 
+  config.output.publicPath = "/"; // 배포된 파일의 경로를 설정
+
+    config.optimization = {
+      ...config.optimization,
+      minimize: true, // 빌드 결과물을 압축
+    };
+
   return config;
 };
+
+// module.exports = function override(config, env) {
+//   // 여기에 원하는 웹팩 설정을 추가하거나 수정합니다.
+//   if (env === 'production') { // 배포 환경
+//     config.devServer = undefined; // 배포 환경에서는 개발 서버 설정 필요 없음
+//     config.output.publicPath = "/"; // 배포된 파일의 경로를 설정
+
+//     config.optimization = {
+//       ...config.optimization,
+//       minimize: true, // 빌드 결과물을 압축
+//     };
+//   }
+//   return config;
+// };  
